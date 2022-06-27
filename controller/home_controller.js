02f716d834529d7360d55dc92264223d1696dfb2
raghav goyal <raghavgoyaldbd4782gmail.com>
2022-06-27 08:21:51 +0530

    add routes and controller

---
+++ controller/home_controller.js
@@ -0,0 +1,5 @@
+module.exports.home=function(req,res){
+    return res.end('<h1>Codial</h1>')
+}
+
+
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+// this module help in setting different routes and modules
+// express is created only once ony instance is passed
+const express = require('express')
+const router = express.Router();
+// noe we acesses function which we want to acesses
+const homecontroller=require('../controller/home_controller')
+router.get('/',homecontroller.home);
+
+
+module.exports=router;
