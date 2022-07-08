a469aedcb9d015ceb3c20c65832fba7f8eec4f88
raghav goyal <raghavgoyaldbd4782gmail.com>
2022-07-08 12:45:53 +0530

    delete comments

--- routes/comments.js
+++ routes/comments.js
@@ -2,7 +2,9 @@ const express=require('express');
 const passport=require('passport');
 const router = express.Router();
 
-const CommentController=require('../controller/comment_controller')
+const CommentController=require('../controller/comment_controller');
+
 
 router.post('/create',passport.checkAuthentication,CommentController.create);
+router.get('/destroy/:id',passport.checkAuthentication,CommentController.destroy);
 module.exports=router; 
